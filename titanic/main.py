38ab0d190820e5131c2553a2ae77f0a168feeff3
popokatapepel <jan-seins@hotmail.de>
2017-12-31 16:54:10 +0100

    jupyter

--- titanic/main.py
+++ titanic/main.py
@@ -67,16 +67,7 @@ y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
 
 
 classifiers = [
-    KNeighborsClassifier(3),
-    SVC(kernel="linear", C=0.025),
-    SVC(gamma=2, C=1),
-    GaussianProcessClassifier(1.0 * RBF(1.0)),
-    DecisionTreeClassifier(max_depth=5),
-    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-    MLPClassifier(alpha=1),
-    AdaBoostClassifier(),
-    GaussianNB(),
-    QuadraticDiscriminantAnalysis()]
+    LogisticRegression(max_iter=1000)]
 
 
 
